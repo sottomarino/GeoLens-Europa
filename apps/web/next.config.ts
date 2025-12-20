733872245020a062d86e31d4be074c7cba2ba3d8
sottomarino <sottomarino@users.noreply.github.com>
2025-12-20 08:27:55 +0100

    Add Firebase Hosting configuration

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   transpilePackages: [
     '@geo-lens/core-geo',
     '@geo-lens/geocube',
